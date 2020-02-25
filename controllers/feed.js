8d24f0e6c497af039dd82777171eec9eb24f39af
kazuki0924 <42589054+kazuki0924@users.noreply.github.com>
2020-02-26 05:27:11 +0900

    381

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 const { validationResult } = require('express-validator/check');
 
 const Post = require('../models/post');
@@ -100,6 +103,19 @@ exports.updatePost = (req, res, next) => {
         error.statusCode = 404;
         throw error;
       }
+      if (imageUrl !== post.imageUrl) {
+        clearImage(post.imageUrl);
+      }
+      post.title = title;
+      post.imageUrl = imageUrl;
+      post.content = content;
+      return post.save();
+    })
+    .then(result => {
+      res.status(200).json({
+        message: 'Post updated!',
+        post: result
+      });
     })
     .catch(err => {
       if (!err.statusCode) {
@@ -107,4 +123,9 @@ exports.updatePost = (req, res, next) => {
       }
       next(err);
     });
+};
+
+const clearImage = filePath => {
+  filePath = path.join(__dirname, '..', filePath);
+  fs.unlink(filePath, err => console.log(err));
 };
