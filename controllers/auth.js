781188cb3c9ccf55804f93d2a836672cfcef53c2
kazuki0924 <42589054+kazuki0924@users.noreply.github.com>
2020-02-29 21:21:50 +0900

    395

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,7 +9,7 @@ exports.signup = (req, res, next) => {
   if (!errors.isEmpty()) {
     const error = new Error('Validation failed.');
     error.statusCode = 422;
-    error.date = errors.array();
+    error.data = errors.array();
     throw error;
   }
   const email = req.body.email;
@@ -26,10 +26,7 @@ exports.signup = (req, res, next) => {
       return user.save();
     })
     .then(result => {
-      res.status(201).json({
-        message: 'User created!',
-        userId: result._id
-      });
+      res.status(201).json({ message: 'User created!', userId: result._id });
     })
     .catch(err => {
       if (!err.statusCode) {
@@ -60,11 +57,55 @@ exports.login = (req, res, next) => {
         throw error;
       }
       const token = jwt.sign(
-        { email: loadedUser.email, userId: loadedUser._id.toString() },
-        'secret',
+        {
+          email: loadedUser.email,
+          userId: loadedUser._id.toString()
+        },
+        'somesupersecretsecret',
         { expiresIn: '1h' }
       );
       res.status(200).json({ token: token, userId: loadedUser._id.toString() });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.getUserStatus = (req, res, next) => {
+  User.findById(req.userId)
+    .then(user => {
+      if (!user) {
+        const error = new Error('User not found.');
+        error.statusCode = 404;
+        throw error;
+      }
+      res.status(200).json({ status: user.status });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.updateUserStatus = (req, res, next) => {
+  const newStatus = req.body.status;
+  User.findById(req.userId)
+    .then(user => {
+      if (!user) {
+        const error = new Error('User not found.');
+        error.statusCode = 404;
+        throw error;
+      }
+      user.status = newStatus;
+      return user.save();
+    })
+    .then(result => {
+      res.status(200).json({ message: 'User updated.' });
     })
     .catch(err => {
       if (!err.statusCode) {
--- models/post.js
+++ models/post.js
@@ -16,8 +16,9 @@ const postSchema = new Schema(
       required: true
     },
     creator: {
-      type: Object,
-      required: String
+      type: Schema.Types.ObjectId,
+      rref: 'User',
+      required: true
     }
   },
   { timestamps: true }
